3a94306640baa4c2f1ed8d5706878822e98b3e31
EliStephenson <elistephenson17@gmail.com>
2023-07-19 19:02:30 -0600

    day two additions with more buttons and recipes

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-// Get the menu button and menu content
-var menuButton = document.getElementById("menuButton");
-var menuContent = document.getElementById("menuContent");
+// Get all the dropdown buttons
+var dropdowns = document.getElementsByClassName("dropdown");
 
-// Add event listener for click
-menuButton.addEventListener("click", function() {
-    if (menuContent.style.maxHeight){
-        menuContent.style.maxHeight = null;
-    } else {
-        menuContent.style.maxHeight = menuContent.scrollHeight + "px";
-    } 
-});
+// Loop through all dropdown buttons to toggle between hiding and showing content
+for (let i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].getElementsByClassName("dropbtn")[0].addEventListener("click", function() {
+        this.nextElementSibling.classList.toggle("show");
+    });
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
