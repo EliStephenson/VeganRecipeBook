9a3477ea1b216faddd5af0cf4ff05e0f44a02549
EliStephenson <elistephenson17@gmail.com>
2023-07-23 19:52:30 -0600

    added tons more recipes and fixed search function

--- script.js
+++ script.js
@@ -20,3 +20,63 @@ window.onclick = function(e) {
         }
     }
 }
+
+
+
+// Your list of recipes
+const recipes = [
+    { title: 'Pumpkin Spice Syrup', url: 'recipes/pumpkin-spice-syrup.html', content: 'pumpkin' },
+    { title: 'Vegan Bacon', url: 'recipes/vegan-bacon.html', content: '...' },
+    { title: 'Basil Aioli', url: 'recipes/basil-aioli.html', content: '...' },
+    // ... rest of your recipes ...
+  ]
+  
+  
+  // Fuse.js options
+  const options = {
+    keys: ['title','content'],
+    includeScore: true,
+    threshold: 0.2
+  }
+  
+  // Create a new instance of Fuse
+  const fuse = new Fuse(recipes, options)
+
+
+  function searchFunction() {
+    // Get the search term from the input field
+    let query = document.getElementById('search').value;
+  
+    // Perform the search
+    let results = fuse.search(query);
+  
+    // Show the results in the 'results' div
+    let resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '';
+  
+    if (results.length > 0) {
+      resultsDiv.style.display = "block";  // make the dropdown visible
+      for (let result of results) {
+        let a = document.createElement('a');
+        a.textContent = result.item.title;
+        a.href = result.item.url;
+        resultsDiv.appendChild(a);
+        resultsDiv.appendChild(document.createElement('br'));
+      }
+    } else {
+      resultsDiv.style.display = "none";  // hide the dropdown if there are no results
+    }
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('#search')) {
+      const dropdowns = document.getElementsByClassName("searchDropdown");
+      for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i]
+        if (openDropdown.style.display === 'block') {
+          openDropdown.style.display = 'none'
+        }
+      }
+    }
+  }
