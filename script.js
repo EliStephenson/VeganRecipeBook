b43a1a0757ad0799cb184d5583deeb853f1730ab
mairinkelly <118467331+mairinkelly@users.noreply.github.com>
2025-04-08 18:26:06 -0400

    try again

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-// // Get all the dropdown buttons
-// var dropdowns = document.getElementsByClassName("dropdown");
+// Get all the dropdown buttons
+var dropdowns = document.getElementsByClassName("dropdown");
 
-// // Loop through all dropdown buttons to toggle between hiding and showing content
-// for (let i = 0; i < dropdowns.length; i++) {
-//     dropdowns[i].getElementsByClassName("dropbtn")[0].addEventListener("click", function() {
-//         this.nextElementSibling.classList.toggle("show");
-//     });
-// }
+// Loop through all dropdown buttons to toggle between hiding and showing content
+for (let i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].getElementsByClassName("dropbtn")[0].addEventListener("click", function() {
+        this.nextElementSibling.classList.toggle("show");
+    });
+}
 
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(e) {
-//     if (!e.target.matches('.dropbtn')) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         for (let i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')) {
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+// // Toggle main dropdown
+// document.querySelectorAll(".dropbtn").forEach(btn => {
+//   btn.addEventListener("click", function(e) {
+//     e.stopPropagation();
+//     const dropdownContent = this.nextElementSibling;
+//     dropdownContent.classList.toggle("show");
+//   });
+// });
+
+// // Toggle submenus
+// document.querySelectorAll(".submenu-btn").forEach(btn => {
+//   btn.addEventListener("click", function(e) {
+//     e.stopPropagation();
+//     const submenu = this.nextElementSibling;
+//     submenu.classList.toggle("show-submenu");
+//   });
+// });
+
+// // Close all dropdowns/submenus if clicking outside
+// window.addEventListener("click", function(e) {
+//   document.querySelectorAll(".dropdown-content").forEach(menu => {
+//     if (!e.target.closest(".dropdown")) {
+//       menu.classList.remove("show");
 //     }
-// }
+//   });
 
-
-// Toggle main dropdown
-document.querySelectorAll(".dropbtn").forEach(btn => {
-  btn.addEventListener("click", function(e) {
-    e.stopPropagation();
-    const dropdownContent = this.nextElementSibling;
-    dropdownContent.classList.toggle("show");
-  });
-});
-
-// Toggle submenus
-document.querySelectorAll(".submenu-btn").forEach(btn => {
-  btn.addEventListener("click", function(e) {
-    e.stopPropagation();
-    const submenu = this.nextElementSibling;
-    submenu.classList.toggle("show-submenu");
-  });
-});
-
-// Close all dropdowns/submenus if clicking outside
-window.addEventListener("click", function(e) {
-  document.querySelectorAll(".dropdown-content").forEach(menu => {
-    if (!e.target.closest(".dropdown")) {
-      menu.classList.remove("show");
-    }
-  });
-
-  document.querySelectorAll(".submenu-content").forEach(menu => {
-    if (!e.target.closest(".dropdown-submenu")) {
-      menu.classList.remove("show-submenu");
-    }
-  });
-});
+//   document.querySelectorAll(".submenu-content").forEach(menu => {
+//     if (!e.target.closest(".dropdown-submenu")) {
+//       menu.classList.remove("show-submenu");
+//     }
+//   });
+// });
 
 
 
